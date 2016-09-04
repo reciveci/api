c34c588ac5cbd9711bb669c62523c790b9a9a923
Jorge Aguilar <jorge.aguilar.ec@gmail.com>
2016-09-04 10:34:03 -0500

    Issue #126: Carga de datos y fotos de los recicladores de la floresta

--- db/schema.rb
+++ db/schema.rb
@@ -48,15 +48,6 @@ ActiveRecord::Schema.define(version: 20160830052518) do
     t.datetime "updated_at"
   end
 
-  create_table "bussines", force: true do |t|
-    t.string   "name"
-    t.string   "address"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "categories", force: true do |t|
     t.string   "name"
     t.text     "description", limit: 255
