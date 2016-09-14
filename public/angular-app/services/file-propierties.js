ec4981d2546e4fc9d2f126e002a7be493616dc81
Jefferson <jefferson@localhost.localdomain>
2016-09-14 10:12:19 -0500

    issue#130: solucion al error initializers bower_rails

---
+++ public/angular-app/services/file-propierties.js
@@ -0,0 +1,10 @@
+separa.factory('$properties',[function(){
+
+  var properties = {
+    ip:"192.168.124.1",
+    port:"3000"
+ 	};
+
+	return properties;
+
+}])
---
+++ public/angular-app/services/recycling-service.js
@@ -0,0 +1,15 @@
+separa.factory('recyclingFactory', ['$resource', function($resource) {
+
+//    $properties
+//  var API_ROOT ='http://'+$properties.ip+':'+$properties.port+'/';
+//  console.log(API_ROOT);
+
+//  return $resource(API_ROOT + "separate.json", {}, {
+  return $resource("http://localhost:3000/separate.json", {}, {
+      separate : {
+          method : "GET",
+          isArray : true,
+          timeout : 5000
+      }
+    });
+} ])
