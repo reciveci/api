a12b681eebdb996da1bdb39c38792362904dd9d1
Jefferson <jefferson@localhost.localdomain>
2016-10-31 23:32:29 -0500

    issue:#133: Cambio de idioma de los mensajes en login y registro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    before_action :set_locale
+#    before_action :set_locale
     #  protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
     before_action :authenticate_user!, if: :devise_controller?
@@ -14,13 +14,14 @@ class ApplicationController < ActionController::Base
 
     # Prevent CSRF attacks by raising an exception.
     # For APIs, you may want to use :null_session instead.
-    def set_locale
-        I18n.locale = params[:locale] || I18n.default_locale
-    end
-
-    def default_url_options(options={})
-        {locale: I18n.locale}
-    end
+    
+#    def set_locale
+#        I18n.locale = params[:locale] || I18n.default_locale
+#    end
+#
+#    def default_url_options(options={})
+#        {locale: I18n.locale}
+#    end
 
     #solicitar autenticacion en todos los metodos a menos que sea en foramto json
     before_action :authenticate_user!, :only => [:index, :show, :new, :edit, :create, :update, :destroy], :unless => [:is_json]
